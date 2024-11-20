21997859ad98c2dd24a30aa55eb5c34cc5a6b9c2
RonkZeDonk <20400064+RonkZeDonk@users.noreply.github.com>
2024-11-19 19:46:21 -0500

    asdf

--- shopping-list/src/App.jsx
+++ shopping-list/src/App.jsx
@@ -1,41 +1,50 @@
-// Step 4. Import React and useState
+import React, { useState } from "react";
+import "./App.css"
 
-// Step 5. Import the App.css file
+export default function App() {
+  const [tasks, setTasks] = useState([]);
+  const [inputValue, setInputValue] = useState("");
 
-// Step 6. Create a functional component called App
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
 
-    // Step 8a. Initialize useState state variables
+  const handleAddTask = () => {
+    if (inputValue.trim()) {
+      setTasks([...tasks, inputValue]);
+      setInputValue("");
+    }
+  };
 
-    
-    // Step 8b. Create arrow function 'handleInputChange to store the current user input
-   
-    
-    // Step 9. Create arrow function 'handleAddTask' to add tasks to the list
-    
+  const handleDeleteTask = (index) => {
+    const newTaskList = tasks.filter((task, i) => (i !== index));
 
-    // Step 11. Create arrow function 'handleDeleteTask' with parameter 'index' to delete tasks
-   
-        // Step 11a. Create a new list of items to keep
+    setTasks(newTaskList);
+  };
 
-        // Step 11b. Set the list to our new list
+  return (
+    <div className="list">
+      <h1>Shopping List</h1>
+      <div className="input">
+        <input
+          type="text"
+          value={inputValue}
+          onChange={handleInputChange}
+          placeholder="Enter an item"
+        />
+        <button onClick={handleAddTask}>Add</button>
+      </div>
 
-    // Step 7. Return the JSX for the App component
-
-        // Step 7a. Create a div with className 'list' and create a site header
-        
-            {/* Step 7b. Create a div with className 'text' and create an input field with:
-            type 'text' 
-            value inputValue
-            onChange set to handleInputChange
-            placeholder to any placeholder text
-            */}
-           
-           
-            {/* Step 10. Display the shopping list array */}
-            
-            
-                {/* Step 10a. Iterate over every task and retrieve its index*/}
-                
-
-
-
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index} className="task-item">
+            {task}
+            <div className="tasks-actions">
+              <button onClick={() => handleDeleteTask(index)}>Remove</button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
